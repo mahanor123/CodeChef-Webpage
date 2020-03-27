2f94f07e00d75272d73975a6e03ee47c2e35ef4c
Your Name <you@example.com>
2020-03-27 14:47:02 +0530

    use flex

---
+++ package.js
@@ -0,0 +1,51 @@
+{"homepage": "http://mahanor123.github.io/CodeChef-Webpage",
+		"name":"codechef"},
+	"scripts":{
+		"predeploy": "npm run build",
+		"deploy" : "gh-pages -d build"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
